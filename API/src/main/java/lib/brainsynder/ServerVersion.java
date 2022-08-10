8ee09fa319713ec227ad11683f334aab673f250b
brainsynder <briansnyder498@gmail.com>
2022-08-10 13:23:57 -0400

    [Automated Task] Added version: v1_19_2

--- API/src/main/java/lib/brainsynder/ServerVersion.java
+++ API/src/main/java/lib/brainsynder/ServerVersion.java
@@ -29,6 +29,7 @@ public enum ServerVersion implements IVersion {
     v1_18_2 ("v1_18_R2"),
     v1_19 ("v1_19_R1"),
     v1_19_1 (v1_19),
+    v1_19_2 (v1_19),
     v1_19_2 (v1_19)
     ; // ---- END ---- //
 
